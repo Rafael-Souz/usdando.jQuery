5d16a2efd91e22f1c117649fc42861680c5ba272
Rafael Silva de Souza <73540128+Rafael-Souz@users.noreply.github.com>
2022-02-02 23:37:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$('h1').fadeOut("3000").fadeIn("3000")//vai fazer um apagão no titulo de 3s
+$('h1').append(" de Pessoas")//vai acrescentar (de pessoas) no h1
+
+$('#btn').on("click",button)
+function button(){
+  $('li').css({'color':'blue'})
+}//function faz a cor do (li) ficar azul
